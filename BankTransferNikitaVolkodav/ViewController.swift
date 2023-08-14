dd3f42f02aae2587d7d225f45d4ea60f7c7206f0
NikitaVolkodav <nikita15.201252@gmail.com>
2023-08-14 22:54:21 +0300

    init-commit

---
+++ BankTransferNikitaVolkodav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankTransferNikitaVolkodav
+//
+//  Created by Nikita Volkodav on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
